1598a22a24ad99a620dc804293350e156e0f148d
Eric Slaghuis <eslaghuis@gmail.com>
2021-09-22 08:35:24 +0200

    Launch and Config for Map Server
    These can be extended to include the rest of the navigation stack

---
+++ launch/map_server.launch.py
@@ -0,0 +1,22 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+def generate_launch_description():
+    ld = LaunchDescription()
+    config = os.path.join(
+        get_package_share_directory('navigation_lite'),
+        'config',
+        'sensors.yaml'
+        )
+        
+    node=Node(
+        package = 'navigation_lite',
+        name = 'map_server',
+        executable = 'map_server',
+        output="screen",
+        emulate_tty=True,
+        parameters = [config]
+    )
+    ld.add_action(node)
+    return ld
