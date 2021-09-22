88e70d50149ee9334cbca360fa2ac408f7ab5fea
Eric Slaghuis <eslaghuis@gmail.com>
2021-09-22 08:32:04 +0200

    Added Map Server

---
+++ src/map_server.cpp
@@ -0,0 +1,191 @@
+// Copyright (c) 2021 Xeni Robotics
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* **********************************************************************
+ * This is the love child between the sensor_pointcloud package 
+ * [https://github.com/eliotlim/sensor_pointcloud] by Eliot Lim (github: @eliotlim)
+ * and ufo_ros https://github.com/UnknownFreeOccupied/ufomap_ros by 
+ * D. Duberg, KTH Royal Institute of Technology.  All elements taken were 
+ * intergrated and rewritten to work in ROS Foxy by Eric Slaghuis (github @slaghuis).
+ * Lim's work (under BSD license) to read sensor transformation from a 
+ * parameter file, was used to populate a UFO Map pointcloud and pupulate an 
+ * UFO map. The UFO map is then published as as a custom message type to the 
+ * navigation lite stack, typically for the planner server.
+ *
+ * Subscribes to a dynamic list of sensor topics [sensor_msgs::msg::Range]
+ * Publishes a UnknownFreeOccupied map [navigation_interfaces::msg::map]
+ * exposes three simple services to Load and save the map, and to clear the 
+ * map.
+ * ***********************************************************************/
+
+#include <functional>
+#include <future>
+#include <memory>
+#include <string>
+#include <sstream>
+
+#include "navigation_lite/sensor_precipitator.h"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
+namespace navigation_lite
+{
+class MapServer : public rclcpp::Node
+{
+public:
+
+  explicit MapServer(const rclcpp::NodeOptions & options)
+  : Node("map_server", options)
+  {
+    using namespace std::placeholders;
+    
+    RCLCPP_INFO(this->get_logger(), "Starting up");
+    
+    // Create simple services
+    load_service = this->create_service<navigation_interfaces::srv::LoadMap>("nav_lite/load_map", std::bind(&MapServer::load_map, this, _1, _2));
+    save_service = this->create_service<navigation_interfaces::srv::SaveMap>("nav_lite/save_map", std::bind(&MapServer::save_map, this, _1, _2));
+    reset_service = this->create_service<navigation_interfaces::srv::Reset>("nav_lite/reset_map", std::bind(&MapServer::reset_map, this, _1, _2));
+   
+    // Read some parameters
+    this->declare_parameter<std::string>("map_frame", "error");
+    this->declare_parameter<std::string>("map_topic", "error");
+    this->declare_parameter("sensors");
+    
+    // Kick off a init routine
+    this->init_timer_ = this->create_wall_timer( 
+      std::chrono::milliseconds(500), 
+      std::bind(&MapServer::init, this) );
+  }
+
+  void init()
+  {
+    // run this timer only once
+    init_timer_->cancel();
+  
+    // read parameters
+    this->get_parameter("map_frame", map_frame_);
+    this->get_parameter("map_topic", map_topic_);
+    RCLCPP_INFO(this->get_logger(), "Map Frame: %s", map_frame_.c_str());
+  
+    rclcpp::Parameter sensor_param("sensors", std::vector<std::string>({}));
+    this->get_parameter("sensors", sensor_param);
+  
+    // Create SensorPrecipitator Object
+    auto node_ptr = shared_from_this();
+    precipitator = std::make_shared<SensorPrecipitator>(node_ptr, map_topic_, map_frame_);
+  
+    sensors = sensor_param.as_string_array(); 
+    if (sensors.size() == 0) RCLCPP_WARN(this->get_logger(), "No Sensors Configured");
+  
+    for (std::vector<std::string>::iterator sensorNameIt = sensors.begin(); sensorNameIt != sensors.end(); ++sensorNameIt) {
+      std::string sensor_topic, sensor_frame;
+    
+      this->declare_parameter<std::string>(*sensorNameIt + ".topic", "error");
+      this->get_parameter(*sensorNameIt + ".topic", sensor_topic);
+
+      this->declare_parameter<std::string>(*sensorNameIt + ".transform.frame", "error");
+      this->get_parameter(*sensorNameIt + ".transform.frame", sensor_frame);
+        
+      RCLCPP_INFO(this->get_logger(), "Sensor Parameters Loaded - Topic: %s Frame: %s ", sensor_topic.c_str(), sensor_frame.c_str());
+    
+      std::shared_ptr<Sensor> s = precipitator->add_sensor(sensor_topic, sensor_frame);
+    
+      bool loadTransform = false;
+      double translation[3] = {0, 0, 0};
+    
+      // load x,y,z parameters
+      for(char c = 'X'; c<='Z'; c++)
+      {
+        std::string paramStr = *sensorNameIt + ".transform.pos" + c;
+        this->declare_parameter<float>(*sensorNameIt + ".transform.pos" + c, 0.0);
+        if (this->get_parameter(paramStr, translation[c-'X'])) {
+          loadTransform = true;
+          RCLCPP_INFO(this->get_logger(), "Loading transform for %s: %f", paramStr.c_str(), translation[c-'X']);
+        }
+      }
+    
+      if (loadTransform) {
+        float roll, pitch, yaw;
+        this->declare_parameter<float>(*sensorNameIt + ".transform/roll", 0.0);
+        this->get_parameter(*sensorNameIt + ".transform/roll",  roll);
+        this->declare_parameter<float>(*sensorNameIt + ".transform/pitch", 0.0);
+        this->get_parameter(*sensorNameIt + ".transform/pitch", pitch);
+        this->declare_parameter<float>(*sensorNameIt + ".transform/yaw", 0.0);
+        this->get_parameter(*sensorNameIt + ".transform/yaw",   yaw);
+      
+        // Load role, pitch and yaw from parameters
+        tf2::Quaternion q;
+        q.setRPY(roll, pitch, yaw);
+      
+        rclcpp::Time now = this->get_clock()->now();
+        geometry_msgs::msg::TransformStamped t;
+
+        t.header.stamp = now;
+        t.header.frame_id = map_frame_;
+        //t.child_frame_id = <<<<<<will be set before message sent
+
+        t.transform.translation.x = translation[0];
+        t.transform.translation.y = translation[1];
+        t.transform.translation.z = translation[2];
+        t.transform.rotation.x = q.x();
+        t.transform.rotation.y = q.y();
+        t.transform.rotation.z = q.z();
+        t.transform.rotation.w = q.w();
+      
+        s->set_transform(t);
+           
+      }
+    }   
+  }
+
+private:
+  // Parameters
+  std::string map_frame_;
+  std::string map_topic_;    
+  std::vector<std::string> sensors = {};
+    
+  std::shared_ptr<SensorPrecipitator> precipitator;
+    
+  rclcpp::TimerBase::SharedPtr init_timer_;
+    
+  rclcpp::Service<navigation_interfaces::srv::LoadMap>::SharedPtr load_service;
+  rclcpp::Service<navigation_interfaces::srv::SaveMap>::SharedPtr save_service;
+  rclcpp::Service<navigation_interfaces::srv::Reset>::SharedPtr   reset_service;
+    
+
+  void save_map(const std::shared_ptr<navigation_interfaces::srv::SaveMap::Request> request,
+          std::shared_ptr<navigation_interfaces::srv::SaveMap::Response> response)
+  {
+    response->success = precipitator->write_to_file(request->filename, request->depth, request->depth);
+  }
+
+  void load_map(const std::shared_ptr<navigation_interfaces::srv::LoadMap::Request> request,
+          std::shared_ptr<navigation_interfaces::srv::LoadMap::Response> response)
+  {
+    response->success = precipitator->load_from_file(request->filename);
+  }
+
+  void reset_map(const std::shared_ptr<navigation_interfaces::srv::Reset::Request> request,
+          std::shared_ptr<navigation_interfaces::srv::Reset::Response> response)
+  {
+    response->success = precipitator->reset(request->new_resolution, request->new_depth_levels); 
+  }
+
+};  // class MapServer
+
+}  // namespace navigation_lite
+
+RCLCPP_COMPONENTS_REGISTER_NODE(navigation_lite::MapServer)
+ 
---
+++ src/sensor_precipitator.cpp
@@ -0,0 +1,198 @@
+// Copyright (c) 2021 Xeni Robotics
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+    Sensor Class
+    Purpose: Instantiates UFO Map and tfListener using a tfBuffer
+    Adapted from the work of 
+    @author Eliot Lim (github: @eliotlim)
+    @version 1.0 (16/5/17)
+*/
+
+#include <navigation_lite/sensor_precipitator.h>
+
+SensorPrecipitator::SensorPrecipitator(rclcpp::Node::SharedPtr node, std::string map_topic, std::string map_frame) :
+                                     node_(node), frame_(map_frame) {
+                                       
+   RCLCPP_DEBUG(node_->get_logger(), "I am here"); 
+                                       
+  // ROS Setup
+  map_publisher_ = node_->create_publisher<navigation_interfaces::msg::UfoMapStamped>(map_topic, 3);
+                                       
+  // Transform listener
+  tf_buffer_ =
+    std::make_unique<tf2_ros::Buffer>(node_->get_clock());
+  transform_listener_ =
+    std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+ 
+  // Transform publisher
+  tf_publisher_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(node_);
+                                       
+                                        
+  // Spin up a new thread
+  float rate = 20.0;
+  std::thread{std::bind(&SensorPrecipitator::execute, this, _1), rate}.detach();                                     
+}
+
+std::shared_ptr<Sensor> SensorPrecipitator::add_sensor(std::string topic, std::string frame) {
+    auto sensor_ptr = std::shared_ptr<Sensor>(new Sensor(node_, topic, frame));
+    sensors.push_back(sensor_ptr);
+    RCLCPP_DEBUG(node_->get_logger(), "Inserted Sensor - Topic: %s, Frame: %s", topic.c_str(), frame.c_str());
+    return sensor_ptr;
+}
+
+void SensorPrecipitator::execute(double rate) {
+  rclcpp::Rate loop_rate(rate);
+  
+  // Create a UFOMap
+  
+  // Build a UFO map
+  // 10 cm voxel size                                     
+  double resolution = 0.1;   //Read from parameter
+                                       
+  // Maximum range to integrate, in meters.
+  // Set to negative value to ignore maximum range.
+  double max_range = 7.0;
+
+  // The depth at which free space should be cleared.
+  // A higher value significantly increases the integration speed
+  // for smaller voxel sizes.
+  ufo::map::DepthType integration_depth = 1;
+  // Will free space at resolution * 2^(integration_depth) voxel size.
+
+  // Some translation [x, y, z]
+  ufo::math::Vector3 translation(0.0, 0.0, 0.0); 
+
+  // Some rotation (w, x, y, z)
+  ufo::math::Quaternion rotation(1.0, 0.0, 0.0, 0.0);
+
+  ufo::math::Pose6 frame_origin(translation, rotation);
+
+  ufo::math::Vector3 sensor_origin(translation);    
+                                       
+  map = std::make_shared<ufo::map::OccupancyMap>(resolution);                                     
+
+  // Point cloud
+  ufo::map::PointCloud cloud;
+
+  // Fill point cloud
+  
+  while (rclcpp::ok()) {
+    //pcl::PointCloud<pcl::PointXYZ>::SharedPtr point_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    //cloud->header.frame_id = frame_;
+    //cloud->height = 1;
+    cloud.clear();
+
+    // Convert all Sensor readings to Points
+    for (std::vector<std::shared_ptr<Sensor>>::iterator sensorIt = sensors.begin(); sensorIt != sensors.end(); ++sensorIt) { 
+        std::shared_ptr<Sensor> sensor = *sensorIt;
+        
+      // Publish sensor transform if available
+      if (sensor->transform_) {
+        sensor->get_transform()->header.stamp = node_->get_clock()->now();
+        tf_publisher_->sendTransform( * sensor->get_transform());
+      }
+
+      // Check Sensor Range Validity
+      if (sensor->get_range() < 0) { continue; }
+
+      // Get StampedTransform for Sensor
+      geometry_msgs::msg::TransformStamped transform;
+      try {
+        // Calling lookupTransform with tf2::TimePointZero
+        // results in the latest available transform
+        // Swapped point_cloud->header.frame_id with "map"
+        transform = tf_buffer_->lookupTransform(frame_,
+                                                   sensor->frame_,
+                                                   tf2::TimePointZero);
+      } catch (tf2::TransformException & ex) {
+        RCLCPP_WARN(node_->get_logger(), "Could not transform %s to %s : %s", 
+                    sensor->frame_.c_str(), "map", ex.what());
+        continue; // skip this reading
+      }
+
+      // Transform the range reading into a point
+      geometry_msgs::msg::PointStamped pt;
+      pt.point.x = sensor->get_range();
+      geometry_msgs::msg::PointStamped point_out;
+
+      tf2::doTransform(pt, point_out, transform);
+
+      // Store the point in cloud
+      //  pcl::PointXYZ pcl_point;
+      //  pcl_point.x = point_out.point.x;
+      //  pcl_point.y = point_out.point.y;
+      //  pcl_point.z = point_out.point.z;
+      std::shared_ptr<ufo::map::Point3> ufo_point = std::make_shared<ufo::map::Point3>();
+      ufo_point->x() = point_out.point.x;
+      ufo_point->y() = point_out.point.y;
+      ufo_point->z() = point_out.point.z;
+      
+      cloud.push_back(*ufo_point);
+
+      //  point_cloud->points.push_back(pcl_point);
+      //  ++(point_cloud->width);
+
+    }
+    
+    // Specify if the point cloud should be transformed in parallel or not.
+    bool parallel = true;
+    // Transform point cloud to correct frame
+    cloud.transform(frame_origin, parallel);
+
+    // Integrate point cloud into UFOMap
+    map->insertPointCloudDiscrete(sensor_origin, cloud, max_range, integration_depth);
+
+    // Compose the map message and publish
+    rclcpp::Time now = node_->get_clock()->now();
+    bool compress = false;
+    
+    ufo::map::DepthType pub_depth = 0;
+    
+    
+    auto message = std::make_shared<navigation_interfaces::msg::UfoMapStamped>();
+    //navigation_interfaces::msg::UfoMapStamped::Ptr message(new navigation_interfaces::msg::UfoMapStamped);
+    //Convert UFOMap to ROS Message
+    if (navigation_interfaces::ufoToMsg(*map, message->map, ufo::geometry::BoundingVolume(), compress, pub_depth, 1, 0)) {
+      message->header.stamp = now;
+      message->header.frame_id = frame_;    // Should be "map"
+      map_publisher_->publish(*message);
+      RCLCPP_DEBUG(node_->get_logger(), "Map published");
+    }  
+    loop_rate.sleep();
+  }
+  
+  
+}
+
+
+bool SensorPrecipitator::load_from_file(std::string filename)
+{
+  // map->emplace<ufo::map::OccupancyMap>(filename);
+  RCLCPP_DEBUG(node_->get_logger(), "Author has o idea how to write the file: %s", filename.c_str());
+  return false;
+}
+
+bool SensorPrecipitator::write_to_file(std::string filename, bool compressed, int depth)
+{
+  map->write(filename, compressed, depth);
+  return true;
+}
+
+bool SensorPrecipitator::reset(double resolution, int depth_levels)
+{
+  map->clear(resolution, depth_levels);
+  return true;
+}
+
